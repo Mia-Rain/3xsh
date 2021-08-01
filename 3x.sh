1c2c3e35cb12b134e9461076801c65dc6f085318
ThatGeekyWeeb <ssfgames13@gmail.com>
2021-08-01 08:07:47 -0400

    finish... ez

--- 3x.sh
+++ 3x.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+num="${1:? Please provide an integer to test the conjecture on...}" || exit 1
+while [ ! "$num" -eq "1" ] || { echo "Loop begins here"; [ ] ;}; do
+  sleep .1
+  [ "$((num%2))" = "0" ] && {
+    printf '%s / 2' "$num"
+    num=$((num/2))
+    printf ' = %s\n' "$num"
+  } || {
+    [ "$((num%2))" != "0" ] && {
+      printf '(%s*3)+1' "$num"
+      num=$((num*3)); : $((num+=1))
+      printf ' = %s\n' "$num"
+    }
+  }
+done
